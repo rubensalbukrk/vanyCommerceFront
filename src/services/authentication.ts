c2cdef0a74482cfceeece85ce9196953a264aa7e
Rubens Albukrk <rubensf4rda@gmail.com>
2024-08-29 21:19:27 -0300

    added: authentication and page login

---
+++ src/services/authentication.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { api } from './api';
+
+interface AuthProps {
+    email: string
+    password: string
+}
+
+export const authentication = async ({ email, password }: AuthProps) => {
+    try {
+        const response = await axios.post(`${api}/authentication`, 
+            { email, password },
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+        .then(response => response.data)
+        if(response.token){
+            return response;
+        }else{
+            return false;
+        }
+        return true
+    } catch (error) {
+        return false;
+    }
+}
