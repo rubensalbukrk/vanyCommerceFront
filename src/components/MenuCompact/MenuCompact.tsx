de1485d25ecc689a2fc907a068d36f6f5b24bb1a
Rubens Albukrk <rubensf4rda@gmail.com>
2024-09-18 18:13:19 -0300

    added Link router in compact menu for mobile

--- src/components/MenuCompact/MenuCompact.tsx
+++ src/components/MenuCompact/MenuCompact.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa'; // Ícones de menu hambúrguer e fechar
-import colors from 'tailwindcss/colors';
+import Link from "next/link";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa"; // Ícones de menu hambúrguer e fechar
+import colors from "tailwindcss/colors";
 
 const CompactMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,24 +12,42 @@ const CompactMenu = () => {
 
   return (
     <div className="relative z-30">
-
-      <button onClick={toggleMenu} className="text-white mt-2 mr-2 focus:outline-none">
-        {isOpen ? <FaTimes size={34} color={colors.green[400]} /> : <FaBars size={34} color={colors.green[400]} />}
+      <button
+        onClick={toggleMenu}
+        className="text-white mt-2 mr-2 focus:outline-none"
+      >
+        {isOpen ? (
+          <FaTimes size={34} color={colors.green[400]} />
+        ) : (
+          <FaBars size={34} color={colors.green[400]} />
+        )}
       </button>
-      
-      <nav className={`absolute top-2 right-12 bg-gray-800 w-48 p-4 transition-transform duration-300 ${isOpen ? 'transform scale-100' : 'transform scale-0'} origin-top-right md:hidden`}>
+
+      <nav
+        className={`absolute top-2 right-12 bg-gray-800 w-48 p-4 transition-transform duration-300 ${
+          isOpen ? "transform scale-100" : "transform scale-0"
+        } origin-top-right md:hidden`}
+      >
         <ul className="space-y-4">
           <li>
-            <a href="/" className="text-white hover:text-gray-300">Inicio</a>
+            <Link href="/">
+              <p className="text-white hover:text-gray-300">Inicio</p>
+            </Link>
           </li>
           <li>
-            <a href="/product" className="text-white hover:text-gray-300">Produtos</a>
+            <Link href="/product">
+              <p className="text-white hover:text-gray-300">Produtos</p>
+            </Link>
           </li>
           <li>
-            <a href="/cart" className="text-white hover:text-gray-300">Carrinho</a>
+            <Link href="/cart">
+              <p className="text-white hover:text-gray-300">Carrinho</p>
+            </Link>
           </li>
           <li>
-            <a href="/login" className="text-white hover:text-gray-300">Login</a>
+            <Link href="/login">
+              <p className="text-white hover:text-gray-300">Login</p>
+            </Link>
           </li>
         </ul>
       </nav>
